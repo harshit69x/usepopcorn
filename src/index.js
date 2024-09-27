bb4b1d20285d903ca8fa487f74543601afb2d3ab
Harshit Mohanty <122545283+harshit69x@users.noreply.github.com>
2024-09-27 23:36:06 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
     <App />
     {/* <StarRating
       maxRating={5}
@@ -14,5 +13,5 @@ root.render(
     <StarRating size={24} color="red" className="test" defaultRating={2} />
 
     <Test /> */}
-  </React.StrictMode>
+ 
 );
